d2cd0cb521d338781675409f54d9be5b3a8bb550
stellal5059 <stellal5059@hstat.org>
2016-10-13 00:58:35 +0000

    done?

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,28 @@
-def division
-    
+def division(num1, num2)
+  return num2 / num1
 end
 
-def argue
-  
+def argue(argument)
+    return "I'm right and your wrong"
 end
 
-def greeting
-
+def greeting(greeting, name)
+    puts "#{greeting}, #{name}"
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(cheese, topping)
+    return "cheese"
 end
 
-def whisper
-    
+def whisper(word)
+    return "#{word.downcase}"
 end
+
+argue("I'm right and your wrong")
+whisper("Hey!")
+pizza_party("cheese")
+division(35, 5).to_i
